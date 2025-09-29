0154f85323a9fc8b007d6520ec4825a7c1ea4d4c
Aristides Miguel <141264424+Aristidesmiguel@users.noreply.github.com>
2025-09-29 16:44:14 +0100

    Teste

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -3,7 +3,7 @@ export  const Home = () => {
     // Aqui é o corpo do componente  elementos javascript
     return (
         //Aqui são elementos JSX ou seja, elementos html dentro do javascript
-        <h1>pagina inicial</h1>
+        <h1>pagina inicial do Aristides</h1>
         
     )
 }
